f71b80467517f0b7604f621f75d1b72082cd571b
vlbxrt <vlbxrtvxrgxs@gmail.com>
2026-01-11 17:24:01 -0500

    Create Subscription.php

---
+++ app/Models/Subscription.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    use HasFactory;
+
+    protected $table = 'subscriptions';
+
+    protected $fillable = [
+        'full_name',
+        'document',
+        'email',
+        'phone',
+    ];
+
+    public function reports()
+    {
+        return $this->hasMany(SubscriptionReport::class, 'subscription_id');
+    }
+}
